77742ed154f98b7dcbaca30f7ad198ccf17a035d
Eduard Castro <eduard.cas07@gmail.com>
2019-10-16 14:12:53 -0500

    Add solution to the -Java Stdin and Stdout I- challenge of introduction practice.

---
+++ Java/Introduction/Java Stdin and Stdout I/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		int myIntOne = scanner.nextInt();
+		int myIntTwo = scanner.nextInt();
+		int myIntThree = scanner.nextInt();
+		scanner.close();
+
+		System.out.println(myIntOne);
+		System.out.println(myIntTwo);
+		System.out.println(myIntThree);
+	}
+
+}
